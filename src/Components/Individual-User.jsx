c02a13842eb89a578de548e42b907b1a959234b8
matthew <mashh@live.co.uk>
2023-12-12 15:23:52 +0000

    component for users/:username

---
+++ src/Components/Individual-User.jsx
@@ -0,0 +1,30 @@
+import { useParams } from 'react-router-dom';
+import { getOneUser } from '../api/getUsers';
+import { useEffect, useState } from 'react';
+const IndividualUser = () => {
+    const { username } = useParams();
+    const [ IsLoading, setIsLoading ] = useState(true)
+    const [ User, setUser ] = useState();
+    useEffect(() => {
+        getOneUser(username)
+        .then((user) => {
+            setUser(user)
+            setIsLoading(false)
+        })
+    }, [])
+    if (IsLoading) {
+        return (
+            <h1>Loading</h1>
+        )
+    }
+    return (
+        <>
+        <section className='container'>
+            <img src={User.avatar_url}/>
+            <p>{User.username}</p>
+            <p>{User.name}</p>
+        </section>
+        </>
+    )
+};
+export default IndividualUser;
