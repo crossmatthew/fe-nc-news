390f4dd62224ea04aa52242928c683be9b346cb6
matthew <mashh@live.co.uk>
2023-12-13 14:37:59 +0000

    additional states to handle errors and vote changes

--- src/Components/Individual-Article.jsx
+++ src/Components/Individual-Article.jsx
@@ -2,14 +2,18 @@ import { useParams, Link } from 'react-router-dom';
 import { getOneArticle } from '../api/getArticles';
 import { useEffect, useState } from 'react';
 import Comments from './Comments';
+import { ArticleVotePlus, ArticleVoteMinus } from "./ArticleVotes";
 const IndividualArticle = () => {
     const { article_id } = useParams();
     const [ IsLoading, setIsLoading ] = useState(true)
     const [ AnArticle, setAnArticle ] = useState();
+    const [ ArticleVotes, setArticleVotes ] = useState();
+    const [ err, setErr ] = useState(null);
     useEffect(() => {
         getOneArticle(article_id)
         .then((article) => {
             setAnArticle(article)
+            setArticleVotes(article.votes)
             setIsLoading(false)
         })
     }, [])
@@ -26,7 +30,7 @@ const IndividualArticle = () => {
         <h1>{AnArticle.title}</h1>
         <img src={AnArticle.article_img_url}/>
         <p>{AnArticle.body}</p>
-        <p>{AnArticle.votes} Votes {AnArticle.comment_count} Comments</p>
+        <p><ArticleVotePlus articleId={AnArticle.article_id} setVotes={setArticleVotes} setErr={setErr}/> {ArticleVotes} Votes {err} <ArticleVoteMinus articleId={AnArticle.article_id} setVotes={setArticleVotes} setErr={setErr}/> {AnArticle.comment_count} Comments</p>
         </article>
         <Comments props={AnArticle} />
         </>
