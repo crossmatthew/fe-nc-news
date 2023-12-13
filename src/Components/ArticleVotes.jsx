910e699a2465285f46e13a37c934e2dd4088544e
matthew <mashh@live.co.uk>
2023-12-13 14:37:24 +0000

    component to handle article votes

---
+++ src/Components/ArticleVotes.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import patchArticlesVotes from '../api/patchArticlesVotes';
+
+const handleVote = (symbol, id, setVotes, setErr) => {
+    let amount = 0
+    const plusOrMinus = symbol === '+' ? amount = 1 : amount = -1
+    setVotes((currVal) => currVal += amount)
+    setErr(null)
+    patchArticlesVotes(id, amount).catch((error => {
+        setVotes((currentCount) => currentCount += -amount);
+        setErr('Failed. Please Try Again.');
+    }))
+    }
+const ArticleVotePlus = ({ articleId, setVotes, setErr }) => {
+    return (
+        <button onClick={() => handleVote('+', articleId, setVotes, setErr)}>+</button>
+    )
+};
+const ArticleVoteMinus = ({ articleId, setVotes, setErr }) => {
+    return (
+        <button onClick={() => handleVote('-', articleId, setVotes, setErr)}>-</button>
+    )
+};
+export { ArticleVotePlus, ArticleVoteMinus };
