d7f816048c5751872bd5f280c6d99930c5a00657
matthew <mashh@live.co.uk>
2023-12-13 14:35:07 +0000

    wrapped app in context provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter} from 'react-router-dom';
-import { UserProvider } from './Contexts/User.jsx';
+import { UserProvider } from './Contexts/User-Context.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
   <UserProvider>
+  <BrowserRouter>
     <App />
-  </UserProvider>
   </BrowserRouter>
+  </UserProvider>
 );
