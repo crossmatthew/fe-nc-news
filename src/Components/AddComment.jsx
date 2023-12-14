63b44c88b7097f4c6594f91ad25b636c8b42a534
matthew <mashh@live.co.uk>
2023-12-14 10:59:00 +0000

    success message, disable submission when comment is posting, require comment to not be blank

--- src/Components/AddComment.jsx
+++ src/Components/AddComment.jsx
@@ -1,13 +1,29 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../Contexts/User-Context";
 import postComments from "../api/postComments";
 const AddComment = (props) => {
+    const [ CommentStatus, setCommentStatus ] = useState();
+    const [ postingComment, setPostingComment ] = useState();
     const { id } = props
     const { User } = useContext(UserContext);
     const handleSubmit = (event) => {
         event.preventDefault()
+        setPostingComment(true)
+        setCommentStatus('Posting Comment')
         const comment = event.target[0].value
         postComments(id, User, comment)
+        .then((res) => {
+            if (res.status === 201) {
+                setPostingComment(false)
+                setCommentStatus('Comment Posted Successfully')
+                setTimeout(() => {
+                    setCommentStatus(null)
+                }, 1000)
+            }
+        })
+        .catch(err => {
+            setCommentStatus(err);
+        });
         event.target.reset()
     }
     if (User) {
@@ -15,8 +31,9 @@ const AddComment = (props) => {
             <section>
                 <form className="container" id="add-comment" onSubmit={handleSubmit}>
                     <p>Logged in as: {User}</p>
-                    <input type="text" placeholder="Share Your Thoughts" id="text-entry"></input>
-                    <input type="submit" value="Comment"></input>
+                    <input type="text" placeholder="Share Your Thoughts" id="text-entry" required={true} minLength={1}></input>
+                    <input type="submit" value="Comment" disabled={postingComment}></input>
+                    <p>{CommentStatus}</p>
                 </form>
             </section>
         )
