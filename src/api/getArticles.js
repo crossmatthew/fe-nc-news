845c7b4bf3c60c615a3fe2bfe3eeebb61c41745c
matthew <mashh@live.co.uk>
2023-12-15 11:43:14 +0000

    promise rejection, error catching in get apis

--- src/api/getArticles.js
+++ src/api/getArticles.js
@@ -1,16 +1,29 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import ErrorHandler from '../Components/ErrorHandler';
 const getArticles = () => {
     return axios
     .get('https://nc-news-js3f.onrender.com/api/articles')
     .then((res) => {
+        if (res.status !== 200) {
+            return Promise.reject({ status:res.status, message: res.statusText })
+        }
         return res.data.articles
+    })
+    .catch((err) => {
+        return Promise.reject({status: err.response.status, msg: err.response.data.msg})
     })
 };
 const getOneArticle = (id) => {
     return axios
     .get(`https://nc-news-js3f.onrender.com/api/articles/${id}`)
-    .then((res) => {
+    .then((res) => {    
+        if (res.status !== 200) {
+            return Promise.reject({ status:res.status, message: res.statusText })
+        }
         return res.data.article
+    })
+    .catch((err) => {
+        return Promise.reject({status: err.response.status, msg: err.response.data.msg})
     })
 };
 export { getArticles, getOneArticle }
