f6fa2ae146d085d0a28416102a09e084406d0e54
matthew <mashh@live.co.uk>
2023-12-28 16:28:04 +0000

    add topics routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Header from './Components/Header';
 import Nav from './Components/Nav';
 import Articles from './Components/Articles';
 import IndividualArticle from './Components/Individual-Article';
+import TopicArticles from './Components/TopicArticles';
+import Topics from './Components/Topics';
 import Users from './Components/Users';
 import IndividualUser from './Components/Individual-User';
 import ErrorHandler from './Components/ErrorHandler';
@@ -16,6 +18,8 @@ function App() {
       <Route path="/" element={ <Articles /> }></Route>
       <Route path="/:article_id" element={ <IndividualArticle /> }></Route>
       <Route path="/users" element= { <Users /> }></Route>
+      <Route path="/topics" element={ <Topics /> }></Route>
+      <Route path="/topics/:topic" element={ <TopicArticles /> }></Route>
       <Route path="/users/:username" element={ <IndividualUser /> }></Route>
       <Route path='*' element={ <ErrorHandler/> }></Route>
     </Routes>
