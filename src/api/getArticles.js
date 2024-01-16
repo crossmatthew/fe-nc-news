3843843cb3a9fd2dc3e13d9b25baf7d80b2690ab
matthew <mashh@live.co.uk>
2024-01-16 15:39:16 +0000

    getArticlesQuery fn complete

--- src/api/getArticles.js
+++ src/api/getArticles.js
@@ -39,13 +39,14 @@ const getOneArticle = (id) => {
     })
 };
 const getArticlesQuery = (query, order) => {
+    order ? order = 'desc' : order = 'asc'
     return axios
     .get(`https://nc-news-js3f.onrender.com/api/articles?sort_by=${query}&order=${order}`)
     .then((res) => {
         if (res.status !== 200) {
             return Promise.reject({ status:res.status, message: res.statusText })
         }
-        return res.data.article
+        return res.data.articles
     })
     .catch((err) => {
         return Promise.reject({status: err.response.status, msg: err.response.data.msg})
