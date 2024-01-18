578fbcb723d843e95604b9d73c8a294394b99662
matthew <mashh@live.co.uk>
2024-01-18 16:14:44 +0000

    inital api get for pagination

--- src/api/getArticles.js
+++ src/api/getArticles.js
@@ -51,4 +51,17 @@ const getArticlesQuery = (query, order) => {
         return Promise.reject({status: err.response.status, msg: err.response.data.msg})
     })
 };
+const getArticlesByPage = (query, order, page, limit) => {
+    return axios
+    .get(`https://nc-news-js3f.onrender.com/api/articles?sort_by=${query}&order=${order}&p=${page}&limit=${limit}`)
+      .then((res) => {
+                if (res.status !== 200) {
+            return Promise.reject({ status:res.status, message: res.statusText })
+        }
+        return res.data.articles
+    })
+    .catch((err) => {
+        return Promise.reject({status: err.response.status, msg: err.response.data.msg})
+    })
+};
 export { getArticles, getOneArticle, getArticlesByTopic, getArticlesQuery }
